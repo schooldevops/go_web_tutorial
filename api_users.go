3e918cfb67e205bfccded252925950aac0920691
배기도 <baekido@baegidoui-MacBookPro.local>
2020-12-11 08:41:01 +0900

    add PathVariable

--- api_users.go
+++ api_users.go
@@ -3,16 +3,27 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func usersHandlers() []handler {
 	return []handler{
+		{path: "/users/{id}/summary/{key}", fun: GetUserSummaryByKey, methods: []string{"GET"}},
 		{path: "/users/{id}", fun: UserByID, methods: []string{"GET"}},
 		{path: "/users/{id}", fun: DeleteUserByID, methods: []string{"DELETE"}},
 		{path: "/users", fun: CreateUser, methods: []string{"POST"}},
 		{path: "/users", fun: AllUsers, methods: []string{"GET"}},
 		{path: "/users", fun: UpdateUser, methods: []string{"PUT"}},
 	}
+}
+
+func GetUserSummaryByKey(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	userID := vars["id"]
+	summaryKey := vars["key"]
+
+	fmt.Fprintf(w, "UserByID :%v, key: %v", userID, summaryKey)
 }
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
@@ -24,7 +35,10 @@ func AllUsers(w http.ResponseWriter, r *http.Request) {
 }
 
 func UserByID(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "UserByID")
+	vars := mux.Vars(r)
+	userID := vars["id"]
+
+	fmt.Fprintf(w, "UserByID :%v", userID)
 }
 
 func UpdateUser(w http.ResponseWriter, r *http.Request) {
