09fa3bc4eb357d885191bfe9306d881c80255dc8
배기도 <baekido@baegidoui-MacBookPro.local>
2020-12-10 15:10:34 +0900

    install gorilla

--- main.go
+++ main.go
@@ -5,18 +5,25 @@ import (
 	"html"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World")
-	})
-
-	http.HandleFunc("/path/value", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Your request path is :%q", html.EscapeString(r.URL.Path))
-	})
+	r := mux.NewRouter()
+	r.HandleFunc("/", HelloWorld)
+	r.HandleFunc("/path/value", ResponsePath)
+	http.Handle("/", r)
 
 	fmt.Println("Server start on port: ", 8080)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World")
+}
+
+func ResponsePath(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Your request path is :%q", html.EscapeString(r.URL.Path))
 }
