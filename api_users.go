a89db3238e14c3a06705c4eea853c341d887b496
배기도 <baekido@baegidoui-MacBookPro.local>
2020-12-12 01:47:33 +0900

    add response module

--- api_users.go
+++ api_users.go
@@ -10,12 +10,18 @@ import (
 func usersHandlers() []handler {
 	return []handler{
 		{path: "/users/{id}/summary/{key}", fun: GetUserSummaryByKey, methods: []string{"GET"}},
+		{path: "/users/secret", fun: NotSupportedFunction, methods: []string{"GET"}},
 		{path: "/users/{id}", fun: UserByID, methods: []string{"GET"}},
 		{path: "/users/{id}", fun: DeleteUserByID, methods: []string{"DELETE"}},
 		{path: "/users", fun: CreateUser, methods: []string{"POST"}},
 		{path: "/users", fun: AllUsers, methods: []string{"GET"}},
 		{path: "/users", fun: UpdateUser, methods: []string{"PUT"}},
+		{path: "/users/secret", fun: NotSupportedFunction, methods: []string{"GET"}},
 	}
+}
+
+func NotSupportedFunction(w http.ResponseWriter, r *http.Request) {
+	responseWithError(w, http.StatusNotImplemented, "Your request not implemented yet.")
 }
 
 func GetUserSummaryByKey(w http.ResponseWriter, r *http.Request) {
@@ -23,7 +29,10 @@ func GetUserSummaryByKey(w http.ResponseWriter, r *http.Request) {
 	userID := vars["id"]
 	summaryKey := vars["key"]
 
-	fmt.Fprintf(w, "UserByID :%v, key: %v", userID, summaryKey)
+	// fmt.Fprintf(w, "UserByID :%v, key: %v", userID, summaryKey)
+	payload := map[string]string{"message": fmt.Sprintf("UserByID :%v, key: %v", userID, summaryKey)}
+	responseWithJSON(w, http.StatusOK, payload)
+
 }
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
